f935edcecda25aa78016142322b3a9d2dcfdcaaf
NoahY <noahy57@gmail.com>
2011-09-17 08:52:23 +0530

    version

--- qa-plugin.php
+++ qa-plugin.php
@@ -4,7 +4,7 @@
         Plugin Name: Buddypress Integration
         Plugin URI: 
         Plugin Description: 
-        Plugin Version: 0.1
+        Plugin Version: 1.0b
         Plugin Date: 2011-08-15
         Plugin Author: NoahY
         Plugin Author URI: 
